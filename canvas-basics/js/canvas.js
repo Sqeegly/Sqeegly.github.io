3eb7f0d56f8697440df6f200ec3b81d8bf61b45b
Sqeegly <chiser@eastsideprep.org>
2022-02-02 09:29:40 -0800

    mit

---
+++ canvas-basics/js/canvas.js
@@ -0,0 +1,44 @@
+//console.log("Hello")
+
+// lets do for loops
+// count 1 - 10
+for (let counter = 1; counter <= 10; counter++) {
+    console.log(counter);
+}
+
+// count 10 - 1
+for (let counter = 10; counter >=1; counter--) {
+    console.log(counter);  
+}
+// count 1 - 10 only odd numbers
+for (let counter = 1; counter <= 11; counter +=2) {
+    console.log(counter);
+}
+
+// get canvas element
+let myCanvas = document.getElementById("my-canvas");
+// create canvas object
+let ctx =myCanvas.getContext("2d");
+
+// draw rect
+ctx.fillStyle = "#FFE4C4"
+ctx.fillRect(0, 0, 200, 100);
+
+// draw lines
+ctx.beginPath();
+ctx.moveTo(0, 0);
+ctx.lineTo(200, 100);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(200, 0)
+ctx.lineTo(0, 100);
+ctx.stroke();
+
+// draw circle
+ctx.beginPath();
+ctx.arc(100, 50, 25, 0, 2 * Math.PI);
+ctx.strokeStyle = "Red";
+ctx.stroke();
+
+// using a for loop, draw a bullseye
