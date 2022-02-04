9ca638acf51af2b608806172101f6e43195db0a0
Sqeegly <chiser@eastsideprep.org>
2022-02-04 14:49:00 -0800

    bounce

--- canvas-animation/js/animation.js
+++ canvas-animation/js/animation.js
@@ -5,7 +5,8 @@ let ctx =myCanvas.getContext("2d");
 
 // object position
 let xPosition = 0;
-
+let yPosition = 0;
+/*
 function moveHorizontal () {
     // clear screen
 ctx.clearRect(0, 0, 500, 500);
@@ -17,5 +18,43 @@ xPosition += 1;
     if (xPosition >= 500){
         xPosition = 0;
     }
+} */
+
+
+/*
+function moveVertical() {
+       // clear screen
+ctx.clearRect(0, 0, 500, 500);
+// draw rect at current position
+ctx.fillRect(0, yPosition, 20, 20);
+// move the x position over by x pixels
+yPosition += 1;
+// did i hit the wall? if so wrap around
+if (yPosition >= 500){
+    yPosition = 0;
 }
-setInterval (moveHorizontal, 10);
+
+}
+*/
+let xDirection = 1
+
+function bounce(){
+    
+
+    
+        // clear screen
+    ctx.clearRect(0, 0, 500, 500);
+        // draw rect at current position
+    ctx.fillRect(xPosition, 0, 20, 20);
+        // move the x position over by x pixels
+    xPosition = xPosition + xDirection;
+        // did i hit the wall? if so wrap around
+        if (xPosition >= 490){
+            xDirection = -1;     
+}
+if (xPosition <= 10) {
+    xDirection = 1;
+}
+}
+
+setInterval (bounce, 10);
