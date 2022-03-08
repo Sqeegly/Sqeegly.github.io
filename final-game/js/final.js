4ed90dbe25d82e14513fa717bffde6ad88bc1c71
Sqeegly <chiser@eastsideprep.org>
2022-03-08 14:14:39 -0800

    Final Final game

--- final-game/js/final.js
+++ final-game/js/final.js
@@ -12,10 +12,11 @@ let playerYDir = 0;
 let playerSpeed = 1.5;
 let playerHeight = 15;
 let playerWidth = 15;
-xPosistion = [385, 105, 105, 100]
-yPosistion = [125, 125, 105, 405]
-widths = [20, 20, 300, 300]
-heights = [300, 250, 20, 20]
+
+xPosistion = [340, 150, 200, 200, 385, 105, 105, 240, 240, 150, 150, 125, 215, 345, 210, 300, 100, 150, 110, 200]
+yPosistion = [105, 165, 200, 200, 125, 125, 105, 250, 235, 240, 300, 360, 360, 305, 300, 275, 405, 240, 200, 260]
+widths =     [15,  200, 200, 15,   20,  20, 300,  15, 120,  15,  90,  90, 140,  15, 100,  15, 300,  65,  65, 55]
+heights =    [75,  15,  15,  75,  300, 250,  20,  10,  15,  70,  15,  15,  15,  70,  15,  40,  20,  15,  15, 15]
 
 
 
@@ -26,11 +27,22 @@ function drawWalls() {
         ctx.fillRect(xPosistion[i], yPosistion[i], widths[i], heights[i]);
     }
 }
+
+function collision(){
+    if (playerX < 100){
+        playerX = 300;
+        playerY = 145;
+        alert("You have finished the maze. Now do it again!")
+    }
+}
+
 function collisionCheckAll() {
     for (let i = 0; i < xPosistion.length; i++) {
         if (playerX + playerWidth >= xPosistion[i] && playerX <= xPosistion[i] + widths[i]) {
             if (playerY + playerHeight >= yPosistion[i] && playerY <= yPosistion[i] + heights[i]) {
                 console.log("collision with block:" + i)
+                playerX = 300;
+                playerY = 145;
             }
         }
     }
@@ -41,8 +53,12 @@ function drawPlayer() {
     ctx.fillRect(playerX, playerY, 20, 15);
     
 }
+
+
+
 function drawMaze() {
     // X, Y, width, length 
+   
     ctx.fillRect(340, 105, 15, 75);
     ctx.fillRect(150, 165, 200, 15);
     ctx.fillRect(200, 200, 200, 15);
@@ -52,12 +68,19 @@ function drawMaze() {
     ctx.fillRect(105, 105, 300, 20);
     ctx.fillRect(240, 250, 15, 10);
     ctx.fillRect(240, 235, 120, 15);
+    ctx.fillRect(150, 240, 15, 70);
+    ctx.fillRect(150, 300, 90, 15);
+    ctx.fillRect(125, 360, 90, 15);
+    ctx.fillRect(215, 360, 140, 15);
+    ctx.fillRect(345, 305, 15, 70);
+    ctx.fillRect(210, 300, 100, 15);
+    ctx.fillRect(300, 275, 15, 40);
 
     ctx.fillRect(100, 405, 300, 20);
     ctx.fillRect(150, 240, 65, 15);
     ctx.fillRect(110, 200, 65, 15);
     ctx.fillRect(200, 260, 55, 15);
-    
+
 
 
     
@@ -84,9 +107,10 @@ function refreshUI() {
     ctx.clearRect(0, 0, 500, 500);
     movePlayer();
     drawPlayer();
-    drawMaze();
-  //  drawWalls();
-  //  collisionCheckAll();
+  // drawMaze();
+  drawWalls();
+  collisionCheckAll();
+  collision();
 
 }
 function keyPressed(event) {
@@ -95,14 +119,14 @@ function keyPressed(event) {
     keydownOutput.innerHTML = "Key down code: " + key;
 
     // move player
-    if (key === 37) {
+    if (key === 65) {
         playerXDir = -1;
-    } else if (key === 39) {
+    } else if (key === 68) {
         playerXDir = 1;
     }
-    if (key === 38) {
+    if (key === 87) {
         playerYDir = -1;
-    } else if (key === 40) {
+    } else if (key === 83) {
         playerYDir = 1;
     }
 }
@@ -114,14 +138,14 @@ function keyReleased(event) {
     keyupOutput.innerHTML = "Key up code: " + key;
 
     // make player stop
-    if (key === 37) {
+    if (key === 65) {
         playerXDir = 0;
-    } else if (key === 39) {
+    } else if (key === 68) {
         playerXDir = 0;
     }
-    if (key === 38) {
+    if (key === 87) {
         playerYDir = 0;
-    } else if (key === 40) {
+    } else if (key === 83) {
         playerYDir = 0;
     }
 }
